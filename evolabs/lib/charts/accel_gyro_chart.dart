656374d53f8b7dd8d910be6f11c2907108f9f092
Sean Chappell <98842466+seanchappell17@users.noreply.github.com>
2023-02-28 10:34:45 -0600

    Updates to formatting on Analysis Screen
    * Changed the Screen's title location
    
    * Changed the body to a SingleChildScrollView, so there's not an overflow problem anymore.

--- evolabs/lib/charts/accel_gyro_chart.dart
+++ evolabs/lib/charts/accel_gyro_chart.dart
@@ -47,148 +47,153 @@ class _AccelGyroChartState extends State<AccelGyroChart> {
             ),
           ),
         ),
-        title: const Text(
-          "Results",
-          style: TextStyle(
-            fontSize: 32,
-            fontWeight: FontWeight.w500,
+        title: const Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            "Results",
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w500,
+            ),
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
-        child: Column(
-          children: [
-            SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              title: ChartTitle(
-                text: 'Accelerometer',
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w400,
-                  color: primaryColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 60),
+          child: Column(
+            children: [
+              SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                title: ChartTitle(
+                  text: 'Accelerometer',
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w400,
+                    color: primaryColor,
+                  ),
                 ),
+                legend: Legend(
+                  isVisible: true,
+                  position: LegendPosition.bottom,
+                ),
+                enableAxisAnimation: true,
+                series: <ChartSeries<AccelerometerData, DateTime>>[
+                  LineSeries<AccelerometerData, DateTime>(
+                    dataSource: widget.accelerometerData,
+                    xValueMapper: (AccelerometerData value, _) => value.getDate,
+                    yValueMapper: (AccelerometerData value, _) =>
+                        value.getValue[0],
+                    name: 'X',
+                    color: highlightColor2,
+                    markerSettings: const MarkerSettings(
+                      isVisible: true,
+                      height: 4,
+                      width: 4,
+                      shape: DataMarkerType.circle,
+                      borderWidth: 3,
+                      borderColor: highlightColor2,
+                    ),
+                  ),
+                  LineSeries<AccelerometerData, DateTime>(
+                    dataSource: widget.accelerometerData,
+                    xValueMapper: (AccelerometerData value, _) => value.getDate,
+                    yValueMapper: (AccelerometerData value, _) =>
+                        value.getValue[1],
+                    name: 'Y',
+                    color: highlightColor1,
+                    markerSettings: const MarkerSettings(
+                      isVisible: true,
+                      height: 4,
+                      width: 4,
+                      shape: DataMarkerType.circle,
+                      borderWidth: 3,
+                      borderColor: highlightColor1,
+                    ),
+                  ),
+                  LineSeries<AccelerometerData, DateTime>(
+                    dataSource: widget.accelerometerData,
+                    xValueMapper: (AccelerometerData value, _) => value.getDate,
+                    yValueMapper: (AccelerometerData value, _) =>
+                        value.getValue[2],
+                    name: 'Z',
+                    color: primaryColor,
+                    markerSettings: const MarkerSettings(
+                      isVisible: true,
+                      height: 4,
+                      width: 4,
+                      shape: DataMarkerType.circle,
+                      borderWidth: 3,
+                      borderColor: secondaryColor,
+                    ),
+                  ),
+                ],
               ),
-              legend: Legend(
-                isVisible: true,
-                position: LegendPosition.bottom,
+              SfCartesianChart(
+                primaryXAxis: CategoryAxis(),
+                title: ChartTitle(
+                  text: 'Gyroscope',
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.w400,
+                    color: primaryColor,
+                  ),
+                ),
+                legend: Legend(
+                  isVisible: true,
+                  position: LegendPosition.bottom,
+                ),
+                enableAxisAnimation: true,
+                series: <ChartSeries<GyroscopeData, DateTime>>[
+                  LineSeries<GyroscopeData, DateTime>(
+                    dataSource: widget.gyroscopeData,
+                    xValueMapper: (GyroscopeData value, _) => value.getDate,
+                    yValueMapper: (GyroscopeData value, _) => value.getValue[0],
+                    name: 'X',
+                    color: highlightColor2,
+                    markerSettings: const MarkerSettings(
+                      isVisible: true,
+                      height: 4,
+                      width: 4,
+                      shape: DataMarkerType.circle,
+                      borderWidth: 3,
+                      borderColor: highlightColor2,
+                    ),
+                  ),
+                  LineSeries<GyroscopeData, DateTime>(
+                    dataSource: widget.gyroscopeData,
+                    xValueMapper: (GyroscopeData value, _) => value.getDate,
+                    yValueMapper: (GyroscopeData value, _) => value.getValue[1],
+                    name: 'Y',
+                    color: highlightColor1,
+                    markerSettings: const MarkerSettings(
+                      isVisible: true,
+                      height: 4,
+                      width: 4,
+                      shape: DataMarkerType.circle,
+                      borderWidth: 3,
+                      borderColor: highlightColor1,
+                    ),
+                  ),
+                  LineSeries<GyroscopeData, DateTime>(
+                    dataSource: widget.gyroscopeData,
+                    xValueMapper: (GyroscopeData value, _) => value.getDate,
+                    yValueMapper: (GyroscopeData value, _) => value.getValue[2],
+                    name: 'Z',
+                    color: primaryColor,
+                    markerSettings: const MarkerSettings(
+                      isVisible: true,
+                      height: 4,
+                      width: 4,
+                      shape: DataMarkerType.circle,
+                      borderWidth: 3,
+                      borderColor: secondaryColor,
+                    ),
+                  ),
+                ],
               ),
-              enableAxisAnimation: true,
-              series: <ChartSeries<AccelerometerData, DateTime>>[
-                LineSeries<AccelerometerData, DateTime>(
-                  dataSource: widget.accelerometerData,
-                  xValueMapper: (AccelerometerData value, _) => value.getDate,
-                  yValueMapper: (AccelerometerData value, _) =>
-                      value.getValue[0],
-                  name: 'X',
-                  color: highlightColor2,
-                  markerSettings: const MarkerSettings(
-                    isVisible: true,
-                    height: 4,
-                    width: 4,
-                    shape: DataMarkerType.circle,
-                    borderWidth: 3,
-                    borderColor: highlightColor2,
-                  ),
-                ),
-                LineSeries<AccelerometerData, DateTime>(
-                  dataSource: widget.accelerometerData,
-                  xValueMapper: (AccelerometerData value, _) => value.getDate,
-                  yValueMapper: (AccelerometerData value, _) =>
-                      value.getValue[1],
-                  name: 'Y',
-                  color: highlightColor1,
-                  markerSettings: const MarkerSettings(
-                    isVisible: true,
-                    height: 4,
-                    width: 4,
-                    shape: DataMarkerType.circle,
-                    borderWidth: 3,
-                    borderColor: highlightColor1,
-                  ),
-                ),
-                LineSeries<AccelerometerData, DateTime>(
-                  dataSource: widget.accelerometerData,
-                  xValueMapper: (AccelerometerData value, _) => value.getDate,
-                  yValueMapper: (AccelerometerData value, _) =>
-                      value.getValue[2],
-                  name: 'Z',
-                  color: primaryColor,
-                  markerSettings: const MarkerSettings(
-                    isVisible: true,
-                    height: 4,
-                    width: 4,
-                    shape: DataMarkerType.circle,
-                    borderWidth: 3,
-                    borderColor: secondaryColor,
-                  ),
-                ),
-              ],
-            ),
-            SfCartesianChart(
-              primaryXAxis: CategoryAxis(),
-              title: ChartTitle(
-                text: 'Gyroscope',
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.w400,
-                  color: primaryColor,
-                ),
-              ),
-              legend: Legend(
-                isVisible: true,
-                position: LegendPosition.bottom,
-              ),
-              enableAxisAnimation: true,
-              series: <ChartSeries<GyroscopeData, DateTime>>[
-                LineSeries<GyroscopeData, DateTime>(
-                  dataSource: widget.gyroscopeData,
-                  xValueMapper: (GyroscopeData value, _) => value.getDate,
-                  yValueMapper: (GyroscopeData value, _) => value.getValue[0],
-                  name: 'X',
-                  color: highlightColor2,
-                  markerSettings: const MarkerSettings(
-                    isVisible: true,
-                    height: 4,
-                    width: 4,
-                    shape: DataMarkerType.circle,
-                    borderWidth: 3,
-                    borderColor: highlightColor2,
-                  ),
-                ),
-                LineSeries<GyroscopeData, DateTime>(
-                  dataSource: widget.gyroscopeData,
-                  xValueMapper: (GyroscopeData value, _) => value.getDate,
-                  yValueMapper: (GyroscopeData value, _) => value.getValue[1],
-                  name: 'Y',
-                  color: highlightColor1,
-                  markerSettings: const MarkerSettings(
-                    isVisible: true,
-                    height: 4,
-                    width: 4,
-                    shape: DataMarkerType.circle,
-                    borderWidth: 3,
-                    borderColor: highlightColor1,
-                  ),
-                ),
-                LineSeries<GyroscopeData, DateTime>(
-                  dataSource: widget.gyroscopeData,
-                  xValueMapper: (GyroscopeData value, _) => value.getDate,
-                  yValueMapper: (GyroscopeData value, _) => value.getValue[2],
-                  name: 'Z',
-                  color: primaryColor,
-                  markerSettings: const MarkerSettings(
-                    isVisible: true,
-                    height: 4,
-                    width: 4,
-                    shape: DataMarkerType.circle,
-                    borderWidth: 3,
-                    borderColor: secondaryColor,
-                  ),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
